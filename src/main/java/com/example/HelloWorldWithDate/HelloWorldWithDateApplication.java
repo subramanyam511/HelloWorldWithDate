b7fe46ec287a03b5e7c8c14ed66832e399254583
Subbu511 <subramanyam.128511@gmail.com>
2022-09-26 17:18:44 +0530

    intial commit

---
+++ src/main/java/com/example/HelloWorldWithDate/HelloWorldWithDateApplication.java
@@ -0,0 +1,13 @@
+package com.example.HelloWorldWithDate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldWithDateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldWithDateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HelloWorldWithDate/HelloWorldWithDateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelloWorldWithDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldWithDateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
